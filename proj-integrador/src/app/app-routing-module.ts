df99efb5990b5143d3c39da1329f509f416b11c3
Luís Gustavo Magosso Obreli <brmagosso@gmail.com>
2025-08-26 01:41:22 +0000

    new commit

--- proj-integrador/src/app/app-routing-module.ts
+++ proj-integrador/src/app/app-routing-module.ts
@@ -4,9 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   
 {path: 'contador',
-loadChildren: () => import('./contador/contador-module').then(m => m.ContadorModule)}
+loadChildren: () => import('./contador/contador-module').then(m => m.ContadorModule)},
+
+{path: 'estrutura',
+loadChildren: () => import('./estrutura/estrutura-module').then(m => m.EstruturaModule)}
 
 ];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
